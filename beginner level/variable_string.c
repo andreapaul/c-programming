c4883b2ad3b56022af3ed3ef412b02de4062f2d3
andreapaul <34640665+andreapaul@users.noreply.github.com>
2017-12-18 06:59:35 +0000

    Create variable_string.c

---
+++ beginner level/variable_string.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+char name[20]="Sachin Ramesh Tendulkar";
+char nick_name="Little Master";
+int runs=18426;
+float avg=44.83;
+int match=463;
+printf("%s%c%d%f%d",name[20],nick_name,runs,avg,match");
+}
