fd1cb95d5101d30d8555f5e1c9e50f10b782fc7d
andreapaul <34640665+andreapaul@users.noreply.github.com>
2017-12-18 07:30:40 +0000

    Update variable_string.c

--- beginner level/variable_string.c
+++ beginner level/variable_string.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 void main()
 {
-char name[20]="Sachin Ramesh Tendulkar";
-char nick_name="Little Master";
+char name[50]="Sachin Ramesh Tendulkar";
+char nick_name[20]="Little Master";
 int runs=18426;
 float avg=44.83;
 int match=463;
-printf("%s%c%d%f%d",name[20],nick_name,runs,avg,match");
+printf("%s\n%s\n%d\n%f\n%d",name,nick_name,runs,avg,match);
 }
