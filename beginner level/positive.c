656aec0fccb3960dc262dd806fa2d5d0286c73a9
andreapaul <34640665+andreapaul@users.noreply.github.com>
2017-12-19 05:13:06 +0000

    Create positive.c

---
+++ beginner level/positive.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("Enter a no");
+scanf("%d",&n);
+if(n>0)
+printf("\n The no is positive");
+else if(n<0)
+printf("\n The no is negative");
+else
+printf("\n The no is 0");
+}
