4b25e75da0b3fb575aeedb2215f21f8f0633d3bb
andreapaul <34640665+andreapaul@users.noreply.github.com>
2017-12-24 07:16:31 -0800

    Create string_caseconversions.java

---
+++ java_assignments/string_caseconversions.java
@@ -0,0 +1,16 @@
+package java_1;
+import java.io.*;
+
+public class String1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String a="Hello";
+String b="Welcome";
+int c=a.charAt(0);
+System.out.println(a.toLowerCase());
+System.out.println(b.toUpperCase());
+System.out.println(c);
+	}
+
+}
