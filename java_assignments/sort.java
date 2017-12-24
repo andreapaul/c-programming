f5fb13f0a05efcc88f526d657b42b28f4e7da48e
andreapaul <34640665+andreapaul@users.noreply.github.com>
2017-12-24 06:53:06 -0800

    Create sort.java

---
+++ java_assignments/sort.java
@@ -0,0 +1,19 @@
+package java_1;
+import java.io.*;
+import java.util.*;
+
+public class Sort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int array[]= {100,55,34,22,134,76,31};
+for(int no:array)
+{System.out.print("Number="+" "+no+'\n');
+	}
+Arrays.sort(array);
+for(int no:array)
+{System.out.println("Sorted array="+no);
+	}
+	}
+
+}
