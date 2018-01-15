a627839e832547d7b6600c244e4f217ff25f5e45
andreapaul <34640665+andreapaul@users.noreply.github.com>
2018-01-15 11:59:06 +0530

    Create count_no_of_spaces.c

---
+++ codekata/count_no_of_spaces.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+	char s[100];
+	int i,j,count=0;
+	scanf("%[^\n]s",s);
+	for(j=0;s[j]!='\0';++j)
+	{
+		
+	}
+	for(i=0;i<j;i++)
+	{
+	
+	if(s[i]==' ')
+	{
+		count++;
+	}
+		
+	}
+	printf("%d",count);
+	return 0;
+}
