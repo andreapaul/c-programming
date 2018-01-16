d04c85b9f8f4190fc0235a93b160e88ba2f4d03c
andreapaul <34640665+andreapaul@users.noreply.github.com>
2018-01-16 14:00:35 +0530

    Create no_between_l_and_r.c

---
+++ codekata/no_between_l_and_r.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdio.h>
+
+int main(void) {
+	int l,r,n;
+	scanf("%d %d %d",&l,&n,&r);
+	
+	if(n>l&&n<r)
+	{
+		
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+	return 0;
+}
