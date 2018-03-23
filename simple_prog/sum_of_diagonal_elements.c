2711f9b230d70f42891e71c98e9b0e5dbd929068
andreapaul <34640665+andreapaul@users.noreply.github.com>
2018-03-23 19:29:51 +0530

    Create sum_of_diagonal_elements.c

---
+++ simple_prog/sum_of_diagonal_elements.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#define pi 3.14
+int main(void)
+{
+	int i,j,m,n;
+int a[100][100],b[100][100],c[100][100],d[100][100];
+scanf("%d",&n);
+scanf("%d",&m);
+for(i=0;i<n;i++)
+{
+	for(j=0;j<m;j++){
+	scanf("%d",&a[i][j]);		
+	}
+
+}
+for(i=0;i<n;i++)
+{
+	for(j=0;j<m;j++){
+	scanf("%d",&b[i][j]);		
+	}
+
+}
+
+for(i=0;i<n;i++)
+{
+for(j=0;j<m;j++){
+	if(i==j){
+	c[i][j]=a[i][j]+b[i][j];
+	}
+	
+}}
+for(i=0;i<n;i++)
+{
+	for(j=0;j<m;j++){
+	printf("%d\t",c[i][j]);		
+	}
+printf("\n");
+}
+
+
+
+	return 0;
+}
